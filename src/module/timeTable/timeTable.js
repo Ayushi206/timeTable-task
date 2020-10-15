e0ae9b8aecbb64c49539b3d3ff425b5ff36992d1
Ayushi206 <ayushirajpara20@gmail.com>
2020-10-15 17:52:34 +0530

    add input field dynamic

--- src/module/timeTable/timeTable.js
+++ src/module/timeTable/timeTable.js
@@ -7,20 +7,71 @@ class Timetable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            numberOfSub:0
+            numberOfSub:0,
+            subjects:[],
+            subjectShow:false,
+            tableShow:false,
+            totalSubjects:[]
         }
     }
 
     componentDidMount() {
     }
-    setData = (e) =>{
-        this.setState({ numberOfSub :e});
+    setData = (type,e,i) =>{
+        const { subjects } = this.state;
+        if(type === "number"){
+        let a =[]
+        for(let i=0;i< e;i++){
+            a.push({subjectName:""});        
+        }
+        this.setState({ numberOfSub :e , subjects : a});
+        }
+        else {
+            subjects[i].subjectName = e;
+            this.setState({subjects})
+        }
+    }
+    addNumber = (e) => {
+        this.setState({ subjectShow : true})
+    }
+    generate = () => {
+        this.setState({tableShow:true})
+        const { totalSubjects } = this.state;
+        this.state.subjects.map((a,i) => 
+            totalSubjects.push({subjectName:a.subjectName})
+        )
+        this.setState({totalSubjects})
     }
     render() {
-        const { numberOfSub } = this.state;
+        console.log(this.state.subjects,this.state.totalSubjects)
+        const { numberOfSub , subjectShow , subjects,tableShow} = this.state;
         return (
             <TimetableWrapper>
-                <input type="number" label="Number of Subject" placeholder="Enter number" onChange={(e) => this.setData(e.target.value)} value={numberOfSub ? numberOfSub : ""} />
+                {!subjectShow ? 
+                <>
+                    <div className="spacing">
+                    <div className="title">Numer of Subject</div>
+                        <input type="number" label="Number of Subject" placeholder="Enter number" onChange={(e) => this.setData("number",e.target.value)} value={numberOfSub ? numberOfSub : ""} />
+                    </div>
+                    <div className="spacing">
+                        <button className="buttonStyle" onClick={(e) => this.addNumber(e)}>Submit</button>
+                    </div>
+                </>
+                :   
+                <>
+                    {subjects.map((a,i) => 
+                    <div className="spacing">
+                        <input label="Number of Subject" placeholder="Enter subject name" onChange={(e) => this.setData("name",e.target.value,i)} value={a.subjectName ? a.subjectName : ""} />
+                    </div>
+                    )}
+                    <div className="spacing">
+                    <button className="buttonStyle" onClick={(e) => this.generate(e)}>Generate</button>
+                    </div>
+                </>
+                }
+                {tableShow ? 
+                    <table></table>
+                :""}
             </TimetableWrapper>
         )
     }
--- src/module/timeTable/timeTableWrapper.js
+++ src/module/timeTable/timeTableWrapper.js
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
 const TimetableWrapper = styled.div`
+
+.buttonStyle{
+    border:1px solid #5179d9;
+    color:#fff;
+    font-size:20px;
+    margin-top: 20px;
+    padding-bottom: 5px;
+    background-color:#5179d9;
+    border-radius:2px;
+}
+.spacing{
+    margin:25px 0px;
+}
+.title{
+    font-size:12px;
+    color:#000;
+}
 `;
 export { TimetableWrapper };
